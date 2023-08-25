868db9ad0e212b5a91dc2fb83efbaff7e741dd3d
Avni chauhan <avnichauhan9182@gmail.com>
2023-08-25 10:09:28 +0530

    first change

--- src/main/java/mypackg/Calculator.java
+++ src/main/java/mypackg/Calculator.java
@@ -1,10 +1,17 @@
 package mypackg;
 
 public class Calculator {
+	public static int sum(int a,int b) {
+		return a+b;
+	}
+	public static int sub(int a,int b) {
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println(sum(4,7));
+System.out.println(sub(4,7));
 	}
 
 }
